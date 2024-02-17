fb8ba9525587f12f62ce641395c0fd892ec38db4
Alex Shyba <alex.shyba@uniform.dev>
2024-02-17 12:52:37 -0800

    feat: dynamic categories

--- src/components/ShopSearch.tsx
+++ src/components/ShopSearch.tsx
@@ -2,28 +2,12 @@ import { Shop } from "@/types";
 import React, { useEffect, useState } from "react";
 import ShopTile from "./Tile";
 
-const categories: Record<string, string> = {
-  All: "",
-  Mode: "aa69067a-8fad-44c9-ba40-7e44ae3b956e",
-  Technologies: "c9003598-9cfb-4496-89d8-623a1fd0ccaf",
-  "Santé & Beauté": "f195650f-57c1-4406-9997-7112c6efa57a",
-  "Magasins alimentaires": "f195650f-57c1-4406-9997-7112c6efa57a",
-  "Modes de vie & divertissement": "475e66a6-4d2e-4469-9bb7-798f521a89d9",
-};
-
-const ShopSearch = () => {
+const ShopSearch = ({ categories }: { categories: Record<string, string> }) => {
   const [searchResults, setSearchResults] = React.useState([]);
   const [searchTerm, setSearchTerm] = React.useState("");
-
-  const filterOptions = Object.keys(categories);
-  const [selectedCategory, setSelectedCategory] = useState(filterOptions[0]);
-
+  const [selectedCategory, setSelectedCategory] = useState("");
   const onChangeHandler = async (event: any) => {
     setSearchTerm(event.target.value);
-    event.preventDefault();
-    const response = await fetch(`/api/search?q=${searchTerm}`);
-    const results = await response.json();
-    setSearchResults(results);
   };
 
   useEffect(() => {
@@ -48,6 +32,7 @@ const ShopSearch = () => {
             onChange={(e) => setSelectedCategory(e.target.value)}
             className="h-full rounded-md border-0 bg-transparent py-0 pl-3 pr-7 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
           >
+            <option value="">All</option>
             {Object.keys(categories)?.map((c, i: number) => (
               <option key={i} value={categories[c] as string}>
                 {c}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,6 @@
-import ShopTile from "@/components/Tile";
 import ShopSearch from "@/components/ShopSearch";
 import { Shop } from "@/types";
-import {
-  getCategories,
-  getShops,
-  getSubCategories,
-} from "@/uniform/contentUtil";
-import { Category } from "@uniformdev/canvas";
+import { getCategories, getShops } from "@/uniform/contentUtil";
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 
 type ShopsRestaurantsPage = {
@@ -15,6 +9,7 @@ type ShopsRestaurantsPage = {
 
 export default function ShopsRestaurants({
   shops,
+  shopCategories,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className="bg-white py-24 sm:py-32">
@@ -23,14 +18,7 @@ export default function ShopsRestaurants({
           <h1 className="mt-2 pb-6 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Shops & Restaurants
           </h1>
-          <ShopSearch />
-          {/* <div className="mx-auto max-w-7xl overflow-hidden px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
-            <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 lg:gap-x-8">
-              {shops?.map((shop: Shop, index: number) => (
-                <ShopTile key={index} {...shop} />
-              ))}
-            </div>
-          </div> */}
+          <ShopSearch categories={shopCategories} />
         </div>
       </div>
     </div>
@@ -38,7 +26,11 @@ export default function ShopsRestaurants({
 }
 
 export const getStaticProps = (async ({ preview }) => {
-  const shops = await getShops(300, Boolean(preview));
+  const shops = await getShops(200, Boolean(preview));
 
-  return { props: { shops } };
+  const shopCategories: Record<string, string> = (
+    await getCategories(100, Boolean(preview))
+  ).reduce((obj, item) => Object.assign(obj, { [item.name]: item.id }), {});
+
+  return { props: { shops, shopCategories } };
 }) satisfies GetStaticProps<ShopsRestaurantsPage>;
